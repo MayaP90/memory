d0477190b4aeceea88edffe9cf5a3aa5fbc109e8
MayaP90 <maya@Mayas-Air.fios-router.home>
2021-04-30 14:24:10 -0400

    Initial commit of all files to the repository

--- app.js
+++ app.js
@@ -2,32 +2,6 @@ let clickedCard = null;
 let preventClick = false;
 let combosFound = 0;
 
-const colors = [
-    'pink',
-    'yellow',
-    'red',
-    'blue',
-    "dgreen",
-    "dblue",
-    "dpink",
-    "green"
-]
-
-const cards = [...document.querySelectorAll('.card')];
-for (let color of colors) {
-    const cardAIndex = parseInt(Math.random() * cards.length)
-    const cardA = cards[cardAIndex]
-    cards.splice(cardAIndex, 1);
-    cardA.className += $,{color} ;
-    cardA.setAttribute('data-color', color);
-    
-    const cardBIndex = parseInt(Math.random() * cards.length)
-    const cardB = cards[cardBIndex]
-    cards.splice(cardBIndex, 1);
-    cardB.className += $,{color}
-    cardB.setAttribute('data-color', color);
-    
-}
 function onCardClicked(e) {
     const target = e.currentTarget;
 
@@ -48,12 +22,12 @@ function onCardClicked(e) {
         clickedCard = target;    
     } else if (clickedCard) {
       if (
-        clickedCard.getAttribute('data-color') !==
+        clickedCard.getAttribute('data-color') ===
         target.getAttribute('data-color')
       ) {
         preventClick = true;
         setTimeout(() => {
-            clicked.className =
+            clickedCard.className =
                 clickedCard.className.replace('done', '').trim() +
                 'color-hidden';
             target.className =
